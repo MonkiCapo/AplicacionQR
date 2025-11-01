fa43bac86a04e132c58355decfba7fcc44b5abec
LizasoainEzequiel <nahojiroElcavernicola@gmail.com>
2025-11-01 10:18:56 -0300

    Empezando QR y Entrada

---
+++ Proyecto/src/CSharp/AppQR.Core/Servicios/IServicios/IEntradaService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppQR.Core.Entidades;
+using AppQR.Core.Dto;
+
+namespace AppQR.Core.Servicios.IServicios
+{
+    public interface IEntradaService
+    {
+        IEnumerable<Entrada> ObtenerEntradas();
+        Entrada ObtenerEntradaPorID(int id);
+    }
+}
